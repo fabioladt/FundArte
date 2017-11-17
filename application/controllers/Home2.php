a3b0eb0372d87bc995862e4a04b38dd8fc5c8300
Fabiola Duarte <josselynfabiola@gmail.com>
2017-11-16 21:11:28 -0600

    first commit

---
+++ application/controllers/Home2.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Home2 extends CI_Controller
+{
+
+	function __construct(){
+
+		parent::__construct();
+		//$this->load->model('Model_Home');
+
+	}
+
+	public function index()
+	{
+
+		$data = array('titulo' => 'FundArte');
+		$this->load->view("estructura/head", $data);
+		$this->load->view("home/index_crud");
+
+	}
+
+}
---
+++ application/views/estructura/head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<title><?=$titulo ?></title>
+
+	<link rel="stylesheet" href="<?=base_url()?>public/css/bootstrap.css">
+
+</head>
