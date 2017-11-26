ca24c050cd15dec583752a0920e1409ad87624b5
iglesias <iglesias9627@gmail.com>
2017-11-26 17:40:11 -0600

    Imagenes de fondo y logo de FundArte Agregado la vista donaciones Agregado el menu de búsqueda

---
+++ application/views/donaciones.php
@@ -0,0 +1,29 @@
+body>
+
+	<div id="titulo">
+		<h1 class="text-center">Donaciones</h1>
+	</div>
+	
+	<div class="container_donaciones">
+		<p id="text_1">Información General</p>
+	</div>
+	
+	<div id="text_2">
+		<p id="text_2"></p>
+	</div>
+	
+	<div class="container_donaciones">
+		<p id="text_1">Planes de Donación</p>
+	</div>
+	
+	<div id="text_2">
+		<p id="text_2"></p>
+	</div>
+	
+	<div class="container_donaciones">
+		<p id="text_1">Formas de Donación</p>
+	</div>
+	
+	<div id="text_2">
+		<p id="text_2"></p>
+	</div>
--- application/views/estructura/head.php
+++ application/views/estructura/head.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
 <html lang="es">
 
 <head>
@@ -12,5 +14,12 @@
 	<title><?=$titulo ?></title>
 
 	<link rel="stylesheet" href="<?=base_url()?>public/css/bootstrap.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+     	  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>plantillas/css/menu.css">     	  
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>plantillas/css/donaciones.css">
+    <link rel="icon" type="image/ico" href="<?= base_url() ?>plantillas/img/fundarte_mini.jpg">
 
 </head>
