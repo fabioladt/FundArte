7b0efe69fb6650eb46ce18badeb2ef6375d62404
iglesias9627 <iglesias9627@gmail.com>
2017-11-30 00:39:05 -0600

    boton menu
    Se ha agregado el boton menu al nav_publico

--- application/views/estructura/nav_publico.php
+++ application/views/estructura/nav_publico.php
@@ -13,6 +13,21 @@
 		</div>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav navbar-right">
+				<li><a href="<?=base_url()?>" class="smoothScroll"><b>Menú</b></a>
+					<ul class="dropdown">
+						<li><a href="<?=base_url()?>" class="smoothScroll">De la fundación</li></a>
+						<li><a href="<?=base_url()?>" class="smoothScroll">Ejes de acción</a>
+							<ul class="dropdown">
+								<li><a href="<?=base_url()?>" class="smoothScroll">Difusión Artística</li></a>
+								<li><a href="<?=base_url()?>" class="smoothScroll">Formación Artística</li></a>
+								<li><a href="<?=base_url()?>" class="smoothScroll">Profesionalización</li></a>
+							</ul>
+						</li>
+						<li><a href="<?=base_url()?>" class="smoothScroll">Eventos</li></a>
+						<li><a href="<?=base_url()?>" class="smoothScroll">Donaciones</li></a>
+						<li><a href="<?=base_url()?>" class="smoothScroll">¡Contactenos!</li></a>
+					</ul>
+				</li>
                 <li><a href="<?=base_url()?>Solar" class="smoothScroll"><b>Solar</b></a></li>
                 <li><a href="<?=base_url()?>Cafeto" class="smoothScroll"><b>Cafeto</b></a></li>
                 <li><a href="<?=base_url()?>Galeria" class="smoothScroll"><b>Eventos</b></a></li>
