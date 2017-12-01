61201a02dbf9a357bb0c3eb524935303c44a4457
Fabiola Duarte <josselynfabiola@gmail.com>
2017-12-01 08:13:32 -0600

    actualizando

--- sentencias.sql
+++ sentencias.sql
@@ -3,3 +3,6 @@ CREATE VIEW solar_vista AS SELECT nombre, fecha_inicio, fecha_fin, imagen
 
 CREATE VIEW cafeto_vista AS SELECT nombre, fecha_inicio, fecha_fin, imagen
   FROM exposicion WHERE fecha_fin >= current_date ORDER BY fecha_inicio;
+
+CREATE VIEW cafeto_vista AS SELECT nombre, fecha_inicio, fecha_fin, imagen
+  FROM exposicion WHERE fecha_fin >= current_date ORDER BY fecha_inicio;
